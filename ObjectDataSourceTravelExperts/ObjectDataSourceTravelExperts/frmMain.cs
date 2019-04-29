c81bc9d2146a664ae78572693985f828fada809a
Lee Neufeld <Lee.Neufeld@edu.sait.ca>
2019-04-29 12:40:00 -0600

    Test comment in frmMain.cs

--- ObjectDataSourceTravelExperts/ObjectDataSourceTravelExperts/frmMain.cs
+++ ObjectDataSourceTravelExperts/ObjectDataSourceTravelExperts/frmMain.cs
@@ -24,6 +24,7 @@ namespace ObjectDataSourceTravelExperts
         {
             packages = Packages_DB.GetPackages();
             packagesDataGridView.DataSource = packages;
+            // comment test
         }
     }
 }
