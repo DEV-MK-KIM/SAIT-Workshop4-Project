15763041273dd9d0b0e651b7d501d8aa35266f84
Min kyo Kim <Minkyo.Kim@edu.sait.ca>
2019-04-30 23:06:48 -0600

    changes on  suppliers and main form codes

---
+++ ObjectDataSourceTravelExperts/ObjectDataSourceTravelExperts/frmSuppliersAdd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TravelExpertsData;
+
+namespace ObjectDataSourceTravelExperts
+{
+    public partial class frmSuppliersAdd : Form
+    {
+        public bool addSupplier;
+        public Suppliers supplier; // current supplier
+        public frmSuppliersAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
