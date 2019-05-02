aace62f73fef0fd26e20090e202898acb2750518
Lee Neufeld <Lee.Neufeld@edu.sait.ca>
2019-05-02 15:03:57 -0600

    changes to package add

--- ObjectDataSourceTravelExperts/ObjectDataSourceTravelExperts/frmAddPackage.Designer.cs
+++ ObjectDataSourceTravelExperts/ObjectDataSourceTravelExperts/frmAddPackage.Designer.cs
@@ -40,10 +40,10 @@
             this.txtPkgBasePrice = new System.Windows.Forms.TextBox();
             this.txtPkgDesc = new System.Windows.Forms.TextBox();
             this.txtPkgName = new System.Windows.Forms.TextBox();
-            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
-            this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
             this.btnAccept = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.txtDateStart = new System.Windows.Forms.TextBox();
+            this.txtDateEnd = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -111,6 +111,7 @@
             // 
             // txtPkgId
             // 
+            this.txtPkgId.Enabled = false;
             this.txtPkgId.Location = new System.Drawing.Point(182, 53);
             this.txtPkgId.Name = "txtPkgId";
             this.txtPkgId.Size = new System.Drawing.Size(200, 20);
@@ -144,20 +145,6 @@
             this.txtPkgName.Size = new System.Drawing.Size(197, 20);
             this.txtPkgName.TabIndex = 11;
             // 
-            // dtpStartDate
-            // 
-            this.dtpStartDate.Location = new System.Drawing.Point(182, 177);
-            this.dtpStartDate.Name = "dtpStartDate";
-            this.dtpStartDate.Size = new System.Drawing.Size(197, 20);
-            this.dtpStartDate.TabIndex = 12;
-            // 
-            // dtpEndDate
-            // 
-            this.dtpEndDate.Location = new System.Drawing.Point(182, 236);
-            this.dtpEndDate.Name = "dtpEndDate";
-            this.dtpEndDate.Size = new System.Drawing.Size(197, 20);
-            this.dtpEndDate.TabIndex = 13;
-            // 
             // btnAccept
             // 
             this.btnAccept.Location = new System.Drawing.Point(53, 291);
@@ -176,15 +163,29 @@
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
+            // txtDateStart
+            // 
+            this.txtDateStart.Location = new System.Drawing.Point(182, 177);
+            this.txtDateStart.Name = "txtDateStart";
+            this.txtDateStart.Size = new System.Drawing.Size(200, 20);
+            this.txtDateStart.TabIndex = 16;
+            // 
+            // txtDateEnd
+            // 
+            this.txtDateEnd.Location = new System.Drawing.Point(179, 239);
+            this.txtDateEnd.Name = "txtDateEnd";
+            this.txtDateEnd.Size = new System.Drawing.Size(200, 20);
+            this.txtDateEnd.TabIndex = 17;
+            // 
             // frmAddPackage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(400, 337);
+            this.Controls.Add(this.txtDateEnd);
+            this.Controls.Add(this.txtDateStart);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnAccept);
-            this.Controls.Add(this.dtpEndDate);
-            this.Controls.Add(this.dtpStartDate);
             this.Controls.Add(this.txtPkgName);
             this.Controls.Add(this.txtPkgDesc);
             this.Controls.Add(this.txtPkgBasePrice);
@@ -219,9 +220,9 @@
         private System.Windows.Forms.TextBox txtPkgBasePrice;
         private System.Windows.Forms.TextBox txtPkgDesc;
         private System.Windows.Forms.TextBox txtPkgName;
-        private System.Windows.Forms.DateTimePicker dtpStartDate;
-        private System.Windows.Forms.DateTimePicker dtpEndDate;
         private System.Windows.Forms.Button btnAccept;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtDateStart;
+        private System.Windows.Forms.TextBox txtDateEnd;
     }
 }
